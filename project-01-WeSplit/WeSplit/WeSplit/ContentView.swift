802c14440c2bc8e2356a691735da993bcaad9b9a
Martin Rist <mlr-github@martinrist.com>
2021-06-11 14:39:43 +0100

    Day 17 - add remaining fields and calculations

--- project-01-WeSplit/WeSplit/WeSplit/ContentView.swift
+++ project-01-WeSplit/WeSplit/WeSplit/ContentView.swift
@@ -15,15 +15,46 @@ struct ContentView: View {
 
   let tipPercentages = [10, 15, 20, 25, 0]
 
+  var totalPerPerson: Double {
+    let peopleCount = Double(numberOfPeople + 2)
+    let tipSelection = Double(tipPercentages[tipPercentage])
+    let orderAmount = Double(checkAmount) ?? 0
+
+    let tipValue = orderAmount / 100 * tipSelection
+    let grandTotal = orderAmount + tipValue
+    let amountPerPerson = grandTotal / peopleCount
+
+    return amountPerPerson
+  }
+
   var body: some View {
-    Form {
-      Section {
-        TextField("Amount", text: $checkAmount)
-          .keyboardType(.decimalPad)
+    NavigationView {
+      Form {
+        Section {
+          TextField("Amount", text: $checkAmount)
+            .keyboardType(.decimalPad)
+
+          Picker("Number of people", selection: $numberOfPeople) {
+            ForEach(2..<100) {
+              Text("\($0) people")
+            }
+          }
+        }
+
+        Section(header: Text("How much tip do you want to leave?")) {
+          Picker("Tip percentage", selection: $tipPercentage) {
+            ForEach(0..<tipPercentages.count) {
+              Text("\(tipPercentages[$0])%")
+            }
+          }
+          .pickerStyle(SegmentedPickerStyle())
+        }
+
+        Section {
+          Text("$\(totalPerPerson, specifier: "%.2f")")
+        }
       }
-      Section {
-        Text("$\(checkAmount)")
-      }
+      .navigationBarTitle("WeSplit")
     }
   }
 }
