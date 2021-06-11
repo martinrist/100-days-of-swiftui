fcff407eac1b1179dcfae0083e50439ec03b530a
Martin Rist <martinrist@Work-MacBook-Pro.local>
2021-06-11 13:41:48 +0100

    Day 16 - Add new empty WeSplit project

---
+++ project-01-WeSplit/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Martin Rist on 11/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
