f94eb1a12d1a2164146dfaaf38a22e0026f08313
Martin Rist <mlr-github@martinrist.com>
2021-06-15 16:37:46 +0100

    Day 20 - Add empty GuessTheFlag project

---
+++ project-02-GuessTheFlag/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Martin Rist on 15/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
