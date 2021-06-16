f7577027e43799107a478b41c2c742de8bcde7d0
Martin Rist <mlr-github@martinrist.com>
2021-06-16 17:41:06 +0100

    Day 23 - Add blank ViewsAndModifiers project

---
+++ project-03-ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Martin Rist on 16/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
