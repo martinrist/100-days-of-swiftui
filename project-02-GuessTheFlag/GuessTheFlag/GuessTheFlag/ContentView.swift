e985af7a96e3e36c56fac0fbff7559e5e8bc94fa
Martin Rist <mlr-github@martinrist.com>
2021-06-16 14:26:16 +0100

    Day 21 - Implement initial UI

--- project-02-GuessTheFlag/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ project-02-GuessTheFlag/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,9 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+
+  var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+  var correctAnswer = Int.random(in: 0...2)
+
   var body: some View {
-    Text("Hello, world!")
-      .padding()
+    ZStack {
+      Color.blue.edgesIgnoringSafeArea(.all)
+      VStack(spacing: 30) {
+
+        VStack {
+          Text("Tap the flag of")
+            .foregroundColor(.white)
+          Text(countries[correctAnswer])
+            .foregroundColor(.white)
+        }
+        ForEach(0 ..< 3) { number in
+          Button(action: {
+            // flag was tapped
+          }) {
+            Image(countries[number])
+              .renderingMode(.original)
+          }
+        }
+        Spacer()
+      }
+    }
+
   }
 }
 
