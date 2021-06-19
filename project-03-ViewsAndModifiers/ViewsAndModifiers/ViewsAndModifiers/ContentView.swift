1274c137d9b6d6aa0a3e14611819d958b8363ded
Martin Rist <mlr-github@martinrist.com>
2021-06-19 11:41:48 +0100

    Day 23 - Add examples of custom ViewModifiers

--- project-03-ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ project-03-ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -7,10 +7,36 @@
 
 import SwiftUI
 
+struct GridStack<Content: View>: View {
+  let rows: Int
+  let columns: Int
+  let content: (Int, Int) -> Content
+
+  var body: some View {
+    VStack {
+      ForEach(0..<rows) { row in
+        HStack {
+          ForEach(0..<columns) { column in
+            content(row, column)
+          }
+        }
+      }
+    }
+  }
+
+  init(rows: Int, columns: Int, @ViewBuilder content: @escaping (Int, Int) -> Content) {
+    self.rows = rows
+    self.columns = columns
+    self.content = content
+  }
+}
+
 struct ContentView: View {
   var body: some View {
-    Text("Hello, world!")
-      .padding()
+    GridStack(rows: 4, columns: 4) { row, col in
+      Image(systemName: "\(row * 4 + col).circle")
+      Text("R\(row) C\(col)")
+    }
   }
 }
 
