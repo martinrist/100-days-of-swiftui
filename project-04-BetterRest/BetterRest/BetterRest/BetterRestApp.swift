a67c1ec4db639a956694599ee1724212207453d3
Martin Rist <mlr-github@martinrist.com>
2021-06-21 16:43:55 +0100

    Day 26 - Add empty BetterRest project

---
+++ project-04-BetterRest/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Martin Rist on 21/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
---
+++ project-04-BetterRest/BetterRest/BetterRest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Martin Rist on 21/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    Text("Hello, world!")
+      .padding()
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
