c388a1dfcb08909108acbaa0fc932e7a90832bd2
Martin Rist <mlr-github@martinrist.com>
2021-06-22 20:36:28 +0100

    Day 29 - Add blank WordScramble project

---
+++ project-05-WordScramble/WordScramble/WordScramble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Martin Rist on 22/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    Text("Hello, world!")
+      .padding()
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
---
+++ project-05-WordScramble/WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Martin Rist on 22/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
