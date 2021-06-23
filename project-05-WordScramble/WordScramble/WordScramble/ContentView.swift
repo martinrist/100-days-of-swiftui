662b3dc8f3d530e95776a5eb1d01e5350c7cebf6
Martin Rist <mlr-github@martinrist.com>
2021-06-23 20:22:37 +0100

    Day 30 - Add basic UI to add new words

--- project-05-WordScramble/WordScramble/WordScramble/ContentView.swift
+++ project-05-WordScramble/WordScramble/WordScramble/ContentView.swift
@@ -8,9 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+
+  @State private var usedWords = [String]()
+  @State private var rootWord = ""
+  @State private var newWord = ""
+
   var body: some View {
-    Text("Hello, world!")
-      .padding()
+    NavigationView {
+      VStack {
+        TextField("Enter your word", text: $newWord, onCommit: addNewWord)
+          .autocapitalization(.none)
+          .textFieldStyle(.roundedBorder)
+          .padding()
+
+        List(usedWords, id: \.self) {
+          Image(systemName: "\($0.count).circle")
+          Text($0)
+        }
+      }
+      .navigationTitle(rootWord)
+    }
+
+  }
+
+  func addNewWord() {
+    let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+
+    guard answer.count > 0 else { return }
+
+    // extra validation here
+
+    usedWords.insert(answer, at:0)
+    newWord = ""
   }
 }
 
