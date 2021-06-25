0e14f352e0cbe5c95a43bd9693f8166b5299c833
Martin Rist <mlr-github@martinrist.com>
2021-06-25 15:36:04 +0100

    Day 33 - add additional example of dragging letters

--- project-06-Animations/Animations/Animations/ContentView.swift
+++ project-06-Animations/Animations/Animations/ContentView.swift
@@ -8,26 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+
+  let letters = Array("Hello SwiftUI")
+  @State private var enabled = false
   @State private var dragAmount = CGSize.zero
 
   var body: some View {
-    LinearGradient(gradient: Gradient(colors: [.yellow, .red]),
-                   startPoint: .topLeading,
-                   endPoint: .bottomTrailing)
-      .frame(width: 200, height: 200)
-      .clipShape(RoundedRectangle(cornerRadius: 10))
-      .offset(dragAmount)
-      .gesture(
-        DragGesture()
-          .onChanged { dragAmount = $0.translation }
-          .onEnded { _ in
-        withAnimation(.spring()) {
-          dragAmount = .zero
-        }
-      })
-
-      // implicit animation
-      // .animation(.spring())
+    HStack(spacing: 0) {
+      ForEach(0..<letters.count) { num in
+        Text(String(letters[num]))
+          .padding(5)
+          .font(.title)
+          .background(enabled ? Color.blue: Color.red)
+          .offset(dragAmount)
+          .animation(.default.delay(Double(num) / 20))
+      }
+    }
+    .gesture(
+      DragGesture()
+        .onChanged { dragAmount = $0.translation }
+        .onEnded { _ in
+      dragAmount = .zero
+      enabled.toggle()
+    })
   }
 
 }
