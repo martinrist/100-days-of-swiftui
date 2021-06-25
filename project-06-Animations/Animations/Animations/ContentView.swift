c5ab82b61f1192dec637fb93f0121855068087e2
Martin Rist <mlr-github@martinrist.com>
2021-06-25 15:22:40 +0100

    Day 33 - Add example of multiple animation modifiers

--- project-06-Animations/Animations/Animations/ContentView.swift
+++ project-06-Animations/Animations/Animations/ContentView.swift
@@ -8,22 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
-
-  @State private var animationAmount = 0.0
+  @State private var enabled = false
 
   var body: some View {
-    Button("Tap me") {
-      withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
-        animationAmount += 360
-      }
+    Button("Tap Me") {
+      enabled.toggle()
     }
-    .padding(50)
-    .background(Color.red)
+    .frame(width: 200, height: 200)
+    .background(enabled ? Color.blue : Color.red)
     .foregroundColor(.white)
-    .clipShape(Circle())
-    .rotation3DEffect(.degrees(animationAmount),
-                      axis: (x: 1, y: 1, z: 0))
+    .animation(.default)
+    .clipShape(RoundedRectangle(cornerRadius: enabled ? 60 : 0))
+    .animation(.interpolatingSpring(stiffness: 10, damping: 1))
   }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
