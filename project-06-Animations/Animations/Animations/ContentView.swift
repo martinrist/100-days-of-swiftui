aaab6682fb5973b120b9813523ee689d476b4f55
Martin Rist <mlr-github@martinrist.com>
2021-06-25 15:00:27 +0100

    Day 32 - Add example of binding animations

--- project-06-Animations/Animations/Animations/ContentView.swift
+++ project-06-Animations/Animations/Animations/ContentView.swift
@@ -11,23 +11,22 @@ struct ContentView: View {
   @State private var animationAmount: CGFloat = 1
 
   var body: some View {
-    Button("Tap Me") {
-//      animationAmount += 1
-    }
-    .padding(50)
-    .background(Color.red)
-    .foregroundColor(.white)
-    .clipShape(Circle())
-    .overlay(Circle()
-              .stroke(Color.red)
-              .scaleEffect(animationAmount)
-              .opacity(Double(2 - animationAmount))
-              .animation(.easeInOut(duration: 1)
-                          .repeatForever(autoreverses: false)
-                        )
-    )
-    .onAppear {
-      self.animationAmount = 2
+    print(animationAmount)
+
+    return VStack {
+
+      Stepper("Scale amount", value: $animationAmount.animation(), in: 1...10)
+
+      Spacer()
+
+      Button("Tap Me") {
+        self.animationAmount += 1
+      }
+      .padding(40)
+      .background(Color.red)
+      .foregroundColor(.white)
+      .clipShape(Circle())
+      .scaleEffect(animationAmount)
     }
   }
 }
