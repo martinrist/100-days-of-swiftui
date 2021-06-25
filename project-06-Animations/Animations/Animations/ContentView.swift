3f7c94d662c05bf7dd297b5ae7182ddb73ca34f0
Martin Rist <mlr-github@martinrist.com>
2021-06-25 15:29:55 +0100

    Day 33 - add example of gesture with animation

--- project-06-Animations/Animations/Animations/ContentView.swift
+++ project-06-Animations/Animations/Animations/ContentView.swift
@@ -8,18 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
-  @State private var enabled = false
+  @State private var dragAmount = CGSize.zero
 
   var body: some View {
-    Button("Tap Me") {
-      enabled.toggle()
-    }
-    .frame(width: 200, height: 200)
-    .background(enabled ? Color.blue : Color.red)
-    .foregroundColor(.white)
-    .animation(.default)
-    .clipShape(RoundedRectangle(cornerRadius: enabled ? 60 : 0))
-    .animation(.interpolatingSpring(stiffness: 10, damping: 1))
+    LinearGradient(gradient: Gradient(colors: [.yellow, .red]),
+                   startPoint: .topLeading,
+                   endPoint: .bottomTrailing)
+      .frame(width: 200, height: 200)
+      .clipShape(RoundedRectangle(cornerRadius: 10))
+      .offset(dragAmount)
+      .gesture(
+        DragGesture()
+          .onChanged { dragAmount = $0.translation }
+          .onEnded { _ in
+        withAnimation(.spring()) {
+          dragAmount = .zero
+        }
+      })
+
+      // implicit animation
+      // .animation(.spring())
   }
 
 }
