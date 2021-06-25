c0753ba6bbe5258297a8116fa788858690a2a046
Martin Rist <mlr-github@martinrist.com>
2021-06-25 14:54:23 +0100

    Day 32 - Demonstrate using animations in `onAppear()`

--- project-06-Animations/Animations/Animations/ContentView.swift
+++ project-06-Animations/Animations/Animations/ContentView.swift
@@ -12,15 +12,23 @@ struct ContentView: View {
 
   var body: some View {
     Button("Tap Me") {
-      animationAmount += 1
+//      animationAmount += 1
     }
     .padding(50)
     .background(Color.red)
     .foregroundColor(.white)
     .clipShape(Circle())
-    .scaleEffect(animationAmount)
-    .blur(radius: (animationAmount - 1) * 3)
-    .animation(.default)
+    .overlay(Circle()
+              .stroke(Color.red)
+              .scaleEffect(animationAmount)
+              .opacity(Double(2 - animationAmount))
+              .animation(.easeInOut(duration: 1)
+                          .repeatForever(autoreverses: false)
+                        )
+    )
+    .onAppear {
+      self.animationAmount = 2
+    }
   }
 }
 
