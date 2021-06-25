eaca1f8ae5f9a3b5dfb51307c9933cf9ba5e2c41
Martin Rist <mlr-github@martinrist.com>
2021-06-25 14:38:56 +0100

    Day 32 - Create blank Animations project

---
+++ project-06-Animations/Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Martin Rist on 25/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
