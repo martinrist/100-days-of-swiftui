06075eb5bb3e48cd57c4dd954665767fcaeec433
Martin Rist <mlr-github@martinrist.com>
2021-07-05 17:09:43 +0100

    Day 37 - Improve expense list contents

--- project-07-iExpense/iExpense/iExpense/ContentView.swift
+++ project-07-iExpense/iExpense/iExpense/ContentView.swift
@@ -42,7 +42,15 @@ struct ContentView: View {
     NavigationView {
       List {
         ForEach(expenses.items) { item in
-          Text(item.name)
+          HStack {
+            VStack(alignment: .leading) {
+              Text(item.name)
+                .font(.headline)
+              Text(item.type)
+            }
+            Spacer()
+            Text("$\(item.amount)")
+          }
         }
         .onDelete(perform: removeItems)
       }
