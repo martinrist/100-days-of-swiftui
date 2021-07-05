bd74ceb9dfa73c07b898f8ab02a20b64eb45e2bf
Martin Rist <mlr-github@martinrist.com>
2021-07-05 17:07:15 +0100

    Day 37 - persist expenses items in `UserDefaults`

--- project-07-iExpense/iExpense/iExpense/ContentView.swift
+++ project-07-iExpense/iExpense/iExpense/ContentView.swift
@@ -7,15 +7,31 @@
 
 import SwiftUI
 
-struct ExpenseItem: Identifiable {
-  let id = UUID()
+struct ExpenseItem: Identifiable, Codable {
+  var id = UUID()
   let name: String
   let type: String
   let amount: Int
 }
 
 class Expenses: ObservableObject {
-  @Published var items = [ExpenseItem]()
+  @Published var items = [ExpenseItem]() {
+    didSet {
+      let encoder = JSONEncoder()
+      if let encoded = try? encoder.encode(items) {
+        UserDefaults.standard.set(encoded, forKey: "Items")
+      }
+    }
+  }
+
+  init() {
+    if let items = UserDefaults.standard.data(forKey: "Items") {
+      let decoder = JSONDecoder()
+      if let decoded = try? decoder.decode([ExpenseItem].self, from: items) {
+        self.items = decoded
+      }
+    }
+  }
 }
 
 struct ContentView: View {
