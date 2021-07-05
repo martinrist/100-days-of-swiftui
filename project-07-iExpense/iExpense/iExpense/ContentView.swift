e7c03e36f6acea09a847818753b7a49a87965c6f
Martin Rist <mlr-github@martinrist.com>
2021-07-05 16:06:06 +0100

    Day 36 - Add blank iExpense project

---
+++ project-07-iExpense/iExpense/iExpense/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Martin Rist on 05/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    Text("Hello, world!")
+      .padding()
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
---
+++ project-07-iExpense/iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Martin Rist on 05/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
