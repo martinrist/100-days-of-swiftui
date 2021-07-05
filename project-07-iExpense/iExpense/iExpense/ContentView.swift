4b7dc7d524ddd3fb999899779ccb6ffab1e54891
Martin Rist <mlr-github@martinrist.com>
2021-07-05 17:15:28 +0100

    Day 38 - Challenge 1 - Add edit / done button

--- project-07-iExpense/iExpense/iExpense/ContentView.swift
+++ project-07-iExpense/iExpense/iExpense/ContentView.swift
@@ -55,7 +55,8 @@ struct ContentView: View {
         .onDelete(perform: removeItems)
       }
       .navigationBarTitle("iExpense")
-      .navigationBarItems(trailing:
+      .navigationBarItems(leading: EditButton(),
+                          trailing:
                             Button(action: {
         showingAddExpense = true
       }) {
