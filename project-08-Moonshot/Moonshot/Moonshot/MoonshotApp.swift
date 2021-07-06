2cd87442ce8e1b192814805c937d5159d606f3d6
Martin Rist <mlr-github@martinrist.com>
2021-07-06 14:55:58 +0100

    Day 39 - Add empty Moonshot project

---
+++ project-08-Moonshot/Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Martin Rist on 06/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
