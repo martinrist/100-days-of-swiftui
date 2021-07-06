8feedcfb72455f806531e7109f3a3963ea07e14d
Martin Rist <mlr-github@martinrist.com>
2021-07-06 20:32:53 +0100

    Day 43 - Add blank Drawing project

---
+++ project-09-Drawing/Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Martin Rist on 06/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
