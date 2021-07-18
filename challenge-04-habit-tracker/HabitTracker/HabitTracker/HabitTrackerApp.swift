9a2e5c749294de1ec317638ad8dca25f264d27a3
Martin Rist <mlr-github@martinrist.com>
2021-07-18 15:59:41 +0100

    Day 47 - Challenge 4 - Add empty HabitTracker project

---
+++ challenge-04-habit-tracker/HabitTracker/HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Martin Rist on 18/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
