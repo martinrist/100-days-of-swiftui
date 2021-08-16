0ee3087bc926fc0821952a9e55a48a992f96288c
Martin Rist <mlr-github@martinrist.com>
2021-08-16 15:19:21 +0100

    Day 49 - Add blank starter project for CupcakeCorner

---
+++ project-10-CupcakeCorner/CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Martin Rist on 16/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
