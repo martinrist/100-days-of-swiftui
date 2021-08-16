b6987fd7a449edb853f81983d57fbdd23956dc37
Martin Rist <mlr-github@martinrist.com>
2021-08-16 16:28:56 +0100

    Day 50 - Add initial model and order screen

---
+++ project-10-CupcakeCorner/CupcakeCorner/CupcakeCorner/AddressView.swift
@@ -0,0 +1,23 @@
+//
+//  AddressView.swift
+//  AddressView
+//
+//  Created by Martin Rist on 16/08/2021.
+//
+
+import SwiftUI
+
+struct AddressView: View {
+
+  @ObservedObject var order: Order
+
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
+}
+
+struct AddressView_Previews: PreviewProvider {
+  static var previews: some View {
+    AddressView(order: Order())
+  }
+}
