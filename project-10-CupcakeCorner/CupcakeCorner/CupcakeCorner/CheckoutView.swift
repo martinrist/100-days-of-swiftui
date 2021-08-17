074a9fd927dcf17a897bdab91f10e55aff22544a
Martin Rist <mlr-github@martinrist.com>
2021-08-17 16:20:56 +0100

    Day 51 - Complete order placement code

--- project-10-CupcakeCorner/CupcakeCorner/CupcakeCorner/CheckoutView.swift
+++ project-10-CupcakeCorner/CupcakeCorner/CupcakeCorner/CheckoutView.swift
@@ -11,6 +11,9 @@ struct CheckoutView: View {
 
   @ObservedObject var order: Order
 
+  @State private var confirmationMessage = ""
+  @State private var showingConfirmation = false
+
   var body: some View {
     GeometryReader { reader in
       ScrollView {
@@ -23,14 +26,43 @@ struct CheckoutView: View {
           Text("Your total is $\(order.cost, specifier: "%.2f")")
             .font(.title)
 
-          Button("Place order") {
-            // place order
-          }
+          Button("Place order", action: placeOrder)
           .padding()
         }
       }
     }
     .navigationBarTitle("Check out", displayMode: .inline)
+    .alert(isPresented: $showingConfirmation) {
+      Alert(title: Text("Thank you"), message: Text(confirmationMessage), dismissButton: .default(Text("OK")))
+    }
+  }
+
+  func placeOrder() {
+    guard let encoded = try? JSONEncoder().encode(order) else {
+      print("Failed to encode order")
+      return
+    }
+
+    let url = URL(string: "https://reqres.in/api/cupcakes")!
+    var request = URLRequest(url: url)
+    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+    request.httpMethod = "POST"
+    request.httpBody = encoded
+
+    URLSession.shared.dataTask(with: request) { data, response, error in
+      guard let data = data else {
+        print("No data in response: \(error?.localizedDescription ?? "Unknown error").")
+        return
+      }
+
+      if let decodedOrder = try? JSONDecoder().decode(Order.self, from: data) {
+        confirmationMessage = "Your order for \(decodedOrder.quantity) x \(Order.types[decodedOrder.type].lowercased()) cupcakes is on its way!"
+        showingConfirmation = true
+      } else {
+        print("Invalid response from server")
+      }
+
+    }.resume()
   }
 }
 
