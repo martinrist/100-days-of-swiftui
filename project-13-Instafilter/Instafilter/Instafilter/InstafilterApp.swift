6c47937b92287827d9108c69125719865082a209
Martin Rist <mlr-github@martinrist.com>
2021-08-24 20:02:52 +0100

    Day 62 - Add empty Instafilter starter project

---
+++ project-13-Instafilter/Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Martin Rist on 24/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
