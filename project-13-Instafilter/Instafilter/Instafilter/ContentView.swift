35893f5f95e9caffd5e661c681385d57472420ee
Martin Rist <mlr-github@martinrist.com>
2021-08-24 20:23:08 +0100

    Day 62 - Add example of custom binding

--- project-13-Instafilter/Instafilter/Instafilter/ContentView.swift
+++ project-13-Instafilter/Instafilter/Instafilter/ContentView.swift
@@ -8,9 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+
+  @State private var blurAmount: CGFloat = 0
+
   var body: some View {
-    Text("Hello, world!")
-      .padding()
+    let blur = Binding<CGFloat>(
+      get: {
+        self.blurAmount
+      }, set: {
+        self.blurAmount = $0
+        print("New value is \(blurAmount)")
+      }
+    )
+
+    return VStack {
+      Text("Hello, world!")
+        .blur(radius: blurAmount)
+
+      Slider(value: blur, in: 0...20)
+    }
   }
 }
 
