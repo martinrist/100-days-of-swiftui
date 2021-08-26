20594fa6c022175fe32a5eab881c3dc7f934c67e
Martin Rist <mlr-github@martinrist.com>
2021-08-26 15:01:54 +0100

    Day 65 - Sketch out initial UI

--- project-13-Instafilter/Instafilter/Instafilter/ContentView.swift
+++ project-13-Instafilter/Instafilter/Instafilter/ContentView.swift
@@ -10,9 +10,54 @@ import CoreImage
 import CoreImage.CIFilterBuiltins
 
 struct ContentView: View {
+  @State private var image: Image?
+  @State private var filterIntensity = 0.5
 
   var body: some View {
-    Text("Hello World")
+    NavigationView {
+      VStack {
+        ZStack {
+          Rectangle()
+            .fill(Color.secondary)
+
+          if let image = image {
+            image
+              .resizable()
+              .scaledToFit()
+          } else {
+            Text("Tap to select a picture")
+              .foregroundColor(.white)
+              .font(.headline)
+          }
+
+          // display the image
+        }
+        .onTapGesture {
+          // select and image
+        }
+
+        HStack {
+          Text("Intensity")
+          Slider(value: $filterIntensity)
+        }
+        .padding(.vertical)
+
+        HStack {
+          Button("Change filter") {
+            // change filter
+          }
+
+          Spacer()
+
+          Button("Save") {
+            // save picture
+          }
+        }
+      }
+      .padding([.horizontal, .bottom])
+      .navigationTitle("Instafilter")
+
+    }
   }
 
 }
