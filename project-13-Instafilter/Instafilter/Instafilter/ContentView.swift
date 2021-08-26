1c8e145c1ce43ece1dbd7c28272878ecf6558477
Martin Rist <mlr-github@martinrist.com>
2021-08-26 14:56:11 +0100

    Day 65 - Reset Instafilter project to blank

--- project-13-Instafilter/Instafilter/Instafilter/ContentView.swift
+++ project-13-Instafilter/Instafilter/Instafilter/ContentView.swift
@@ -9,44 +9,12 @@ import SwiftUI
 import CoreImage
 import CoreImage.CIFilterBuiltins
 
-
-class ImageSaver: NSObject {
-  func writeToPhotosAlbum(image: UIImage) {
-    UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveError), nil)
-  }
-
-  @objc func saveError(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
-    print("Save finished!")
-  }
-}
-
 struct ContentView: View {
 
-  @State private var image: Image?
-  @State private var showingImagePicker = false
-  @State private var inputImage: UIImage?
-
   var body: some View {
-    VStack {
-      image?
-        .resizable()
-        .scaledToFit()
-
-      Button("Select Image") {
-        showingImagePicker = true
-      }
-    }
-    .sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
-      ImagePicker(image: $inputImage)
-    }
+    Text("Hello World")
   }
 
-  func loadImage() {
-    guard let inputImage = inputImage else { return }
-    image = Image(uiImage: inputImage)
-    let imageSaver = ImageSaver()
-    imageSaver.writeToPhotosAlbum(image: inputImage)
-  }
 }
 
 struct ContentView_Previews: PreviewProvider {
