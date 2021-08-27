6de6dccd4fb22b92e5b61dfb6f39e1754c9d76d7
Martin Rist <mlr-github@martinrist.com>
2021-08-27 15:29:38 +0100

    Day 68 - Demonstrate Comparable and `.sorted()`

--- project-14-BucketList/BucketList/BucketList/ContentView.swift
+++ project-14-BucketList/BucketList/BucketList/ContentView.swift
@@ -7,10 +7,28 @@
 
 import SwiftUI
 
+struct User: Identifiable, Comparable {
+  static func < (lhs: User, rhs: User) -> Bool {
+    lhs.lastName < rhs.lastName
+  }
+
+  let id = UUID()
+  let firstName: String
+  let lastName: String
+}
+
+
 struct ContentView: View {
+  let users = [
+    User(firstName: "Arnold", lastName: "Rimmer"),
+    User(firstName: "Kristine", lastName: "Kochanski"),
+    User(firstName: "David", lastName: "Lister")
+  ].sorted()
+
   var body: some View {
-    Text("Hello, world!")
-      .padding()
+    List(users) { user in
+      Text("\(user.lastName), \(user.firstName)")
+    }
   }
 }
 
