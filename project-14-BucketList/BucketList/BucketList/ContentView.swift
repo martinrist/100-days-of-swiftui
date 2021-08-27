b0a3606d3eeb3b68e7851036de8e54507575fba9
Martin Rist <mlr-github@martinrist.com>
2021-08-27 16:24:34 +0100

    Day 69 - Add example of using FaceID for authentication

--- project-14-BucketList/BucketList/BucketList/ContentView.swift
+++ project-14-BucketList/BucketList/BucketList/ContentView.swift
@@ -6,12 +6,42 @@
 //
 
 import SwiftUI
+import LocalAuthentication
 
 struct ContentView: View {
+  @State private var isUnlocked = false
 
   var body: some View {
-    MapView()
-      .edgesIgnoringSafeArea(.all)
+    VStack {
+      if isUnlocked {
+        Text("Unlocked")
+      } else {
+        Text("Locked")
+      }
+    }
+    .onAppear(perform: authenticate)
+  }
+
+  func authenticate() {
+    let context = LAContext()
+    var error: NSError?
+
+    if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+      let reason = "We need to unlock your data."
+
+      context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics,
+                             localizedReason: reason) { success, authenticationError in
+        DispatchQueue.main.async {
+          if success {
+            isUnlocked = true
+          } else {
+            // there was a problem
+          }
+        }
+      }
+    } else {
+      // no biometrics
+    }
   }
 }
 
