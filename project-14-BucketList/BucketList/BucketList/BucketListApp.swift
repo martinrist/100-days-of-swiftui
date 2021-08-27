abdedbbe92f43ad5297e54385d6bb99cbadfea2a
Martin Rist <mlr-github@martinrist.com>
2021-08-27 15:21:35 +0100

    Day 68 - Add blank BucketList starter app

---
+++ project-14-BucketList/BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Martin Rist on 27/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
