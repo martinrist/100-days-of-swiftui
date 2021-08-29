3fb8d035b8660f6e10a75c1a9e361ca5d31bb32b
Martin Rist <mlr-github@martinrist.com>
2021-08-29 15:54:17 +0100

    Day 72 - Add CodableMKPointAnnotation to save pins

--- project-14-BucketList/BucketList/BucketList/ContentView.swift
+++ project-14-BucketList/BucketList/BucketList/ContentView.swift
@@ -11,7 +11,7 @@ import MapKit
 struct ContentView: View {
 
   @State private var centerCoordinate = CLLocationCoordinate2D()
-  @State private var locations = [MKPointAnnotation]()
+  @State private var locations = [CodableMKPointAnnotation]()
   @State private var selectedPlace: MKPointAnnotation?
   @State private var showingPlaceDetails = false
   @State private var showingEditScreen = false
@@ -33,9 +33,10 @@ struct ContentView: View {
         HStack {
           Spacer()
           Button(action: {
-            let newLocation = MKPointAnnotation()
+            let newLocation = CodableMKPointAnnotation()
             newLocation.coordinate = centerCoordinate
             newLocation.title = "Example Location"
+            newLocation.subtitle = "Description"
             locations.append(newLocation)
 
             selectedPlace = newLocation
@@ -60,10 +61,36 @@ struct ContentView: View {
         showingEditScreen = true
       })
     }
-    .sheet(isPresented: $showingEditScreen) {
+    .sheet(isPresented: $showingEditScreen, onDismiss: saveData) {
       if let selectedPlace = selectedPlace {
         EditView(placemark: selectedPlace)
       }
+    }
+    .onAppear(perform: loadData)
+  }
+
+  func getDocumentsDirectory() -> URL {
+    let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+    return paths[0]
+  }
+
+  func loadData() {
+    let filename = getDocumentsDirectory().appendingPathComponent("SavedPlaces")
+    do {
+      let data = try Data(contentsOf: filename)
+      locations = try JSONDecoder().decode([CodableMKPointAnnotation].self, from: data)
+    } catch {
+      print("Unable to load saved data: \(error.localizedDescription)")
+    }
+  }
+
+  func saveData() {
+    do {
+      let filename = getDocumentsDirectory().appendingPathComponent("SavedPlaces")
+      let data = try JSONEncoder().encode(locations)
+      try data.write(to: filename, options: [.atomicWrite, .completeFileProtection])
+    } catch {
+      print("Unable to save data")
     }
   }
 }
