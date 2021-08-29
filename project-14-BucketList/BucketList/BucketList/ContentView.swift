8af46ba98151c28028d5d31b20c3b2a9b0a6d2af
Martin Rist <mlr-github@martinrist.com>
2021-08-29 16:15:31 +0100

    Day 73 - BucketList Challenge 2 - extract UnlockedMapView

--- project-14-BucketList/BucketList/BucketList/ContentView.swift
+++ project-14-BucketList/BucketList/BucketList/ContentView.swift
@@ -10,60 +10,13 @@ import MapKit
 import LocalAuthentication
 
 struct ContentView: View {
-
-  @State private var centerCoordinate = CLLocationCoordinate2D()
-  @State private var locations = [CodableMKPointAnnotation]()
-  @State private var selectedPlace: MKPointAnnotation?
-  @State private var showingPlaceDetails = false
-  @State private var showingEditScreen = false
   @State private var isUnlocked = false
   @State private var showingAuthErrorAlert = false
 
   var body: some View {
     ZStack {
       if isUnlocked {
-        MapView(centerCoordinate: $centerCoordinate,
-                selectedPlace: $selectedPlace,
-                showingPlaceDetails: $showingPlaceDetails,
-                annotations: locations)
-          .edgesIgnoringSafeArea(.all)
-        Circle()
-          .fill(Color.blue)
-          .opacity(0.3)
-          .frame(width: 32, height: 32)
-
-        VStack {
-          Spacer()
-          HStack {
-            Spacer()
-            Button(action: {
-              let newLocation = CodableMKPointAnnotation()
-              newLocation.coordinate = centerCoordinate
-              newLocation.title = "Example Location"
-              newLocation.subtitle = "Description"
-              locations.append(newLocation)
-
-              selectedPlace = newLocation
-              showingEditScreen = true
-            }) {
-              Image(systemName: "plus")
-            }
-            .padding()
-            .background(Color.black.opacity(0.75))
-            .foregroundColor(.white)
-            .font(.title)
-            .clipShape(Circle())
-            .padding(.trailing)
-          }
-        }
-        .alert(isPresented: $showingPlaceDetails) {
-          Alert(title: Text(selectedPlace?.title ?? "Unknown"),
-                message: Text(selectedPlace?.subtitle ?? "Missing place information"),
-                primaryButton: .default(Text("OK")),
-                secondaryButton: .default(Text("Edit")) {
-            showingEditScreen = true
-          })
-        }
+        UnlockedMapView()
       } else {
         Button("Unlock Places") {
           authenticate()
@@ -78,6 +31,77 @@ struct ContentView: View {
                 dismissButton: .default(Text("OK")))
         }
       }
+    }
+  }
+
+  func authenticate() {
+    let context = LAContext()
+    var error: NSError?
+
+    if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+      let reason = "Please authenticate yourself to unlock your places."
+      context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+        DispatchQueue.main.async {
+          if success {
+            isUnlocked = true
+          } else {
+            showingAuthErrorAlert = true
+          }
+        }
+      }
+    }
+  }
+}
+
+struct UnlockedMapView: View {
+  @State private var centerCoordinate = CLLocationCoordinate2D()
+  @State private var selectedPlace: MKPointAnnotation?
+  @State private var showingPlaceDetails = false
+  @State private var locations = [CodableMKPointAnnotation]()
+  @State private var showingEditScreen = false
+
+  var body: some View {
+    MapView(centerCoordinate: $centerCoordinate,
+            selectedPlace: $selectedPlace,
+            showingPlaceDetails: $showingPlaceDetails,
+            annotations: locations)
+      .edgesIgnoringSafeArea(.all)
+    Circle()
+      .fill(Color.blue)
+      .opacity(0.3)
+      .frame(width: 32, height: 32)
+
+    VStack {
+      Spacer()
+      HStack {
+        Spacer()
+        Button(action: {
+          let newLocation = CodableMKPointAnnotation()
+          newLocation.coordinate = centerCoordinate
+          newLocation.title = "Example Location"
+          newLocation.subtitle = "Description"
+          locations.append(newLocation)
+
+          selectedPlace = newLocation
+          showingEditScreen = true
+        }) {
+          Image(systemName: "plus")
+        }
+        .padding()
+        .background(Color.black.opacity(0.75))
+        .foregroundColor(.white)
+        .font(.title)
+        .clipShape(Circle())
+        .padding(.trailing)
+      }
+    }
+    .alert(isPresented: $showingPlaceDetails) {
+      Alert(title: Text(selectedPlace?.title ?? "Unknown"),
+            message: Text(selectedPlace?.subtitle ?? "Missing place information"),
+            primaryButton: .default(Text("OK")),
+            secondaryButton: .default(Text("Edit")) {
+        showingEditScreen = true
+      })
     }
     .sheet(isPresented: $showingEditScreen, onDismiss: saveData) {
       if let selectedPlace = selectedPlace {
@@ -112,23 +136,6 @@ struct ContentView: View {
     }
   }
 
-  func authenticate() {
-    let context = LAContext()
-    var error: NSError?
-
-    if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
-      let reason = "Please authenticate yourself to unlock your places."
-      context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
-        DispatchQueue.main.async {
-          if success {
-            isUnlocked = true
-          } else {
-            showingAuthErrorAlert = true
-          }
-        }
-      }
-    }
-  }
 }
 
 struct ContentView_Previews: PreviewProvider {
