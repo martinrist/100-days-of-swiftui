2d354071746a6aa7224067e4b096a4a13dca314e
Martin Rist <mlr-github@martinrist.com>
2021-08-31 13:06:57 +0100

    Day 74 - Convert first example to dedicated view

---
+++ project-15-AccessibilityProject/AccessibilityProject/AccessibilityProject/AccessibilityLabelExampleView.swift
@@ -0,0 +1,43 @@
+//
+//  AccessibilityLabelExampleView.swift
+//  AccessibilityLabelExampleView
+//
+//  Created by Martin Rist on 31/08/2021.
+//
+
+import SwiftUI
+
+struct AccessibilityLabelExampleView: View {
+  let pictures = [
+    "ales-krivec-15949",
+    "galina-n-189483",
+    "kevin-horstmann-141705",
+    "nicolas-tissot-335096"
+  ]
+
+  let labels = [
+    "tulips",
+    "Frozen tree buds",
+    "Sunflowers",
+    "Fireworks"]
+
+  @State private var selectedPicture = Int.random(in: 0...3)
+
+  var body: some View {
+    Image(pictures[selectedPicture])
+      .resizable()
+      .scaledToFit()
+      .onTapGesture {
+        selectedPicture = Int.random(in: 0...3)
+      }
+      .accessibilityLabel(labels[selectedPicture])
+      .accessibilityAddTraits(.isButton)
+      .accessibilityRemoveTraits(.isImage)
+  }
+}
+
+struct AccessibilityLabelExampleView_Previews: PreviewProvider {
+  static var previews: some View {
+    AccessibilityLabelExampleView()
+  }
+}
--- project-15-AccessibilityProject/AccessibilityProject/AccessibilityProject/ContentView.swift
+++ project-15-AccessibilityProject/AccessibilityProject/AccessibilityProject/ContentView.swift
@@ -9,31 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
 
-  let pictures = [
-    "ales-krivec-15949",
-    "galina-n-189483",
-    "kevin-horstmann-141705",
-    "nicolas-tissot-335096"
-  ]
-
-  let labels = [
-    "tulips",
-    "Frozen tree buds",
-    "Sunflowers",
-    "Fireworks"]
-
-  @State private var selectedPicture = Int.random(in: 0...3)
-
   var body: some View {
-    Image(pictures[selectedPicture])
-      .resizable()
-      .scaledToFit()
-      .onTapGesture {
-        selectedPicture = Int.random(in: 0...3)
+    NavigationView {
+      List {
+        NavigationLink(destination: AccessibilityLabelExampleView()) {
+          Text("Accessibility Label example")
+        }
       }
-      .accessibilityLabel(labels[selectedPicture])
-      .accessibilityAddTraits(.isButton)
-      .accessibilityRemoveTraits(.isImage)
+      .navigationTitle("Accessibility")
+    }
   }
 }
 
