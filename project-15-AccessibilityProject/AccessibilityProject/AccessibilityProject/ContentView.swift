7087789e079ee7ba57b0f6d9d8536d6fbe714741
Martin Rist <mlr-github@martinrist.com>
2021-08-31 12:51:06 +0100

    Day 74 - Add pictures and display random pictures

--- project-15-AccessibilityProject/AccessibilityProject/AccessibilityProject/ContentView.swift
+++ project-15-AccessibilityProject/AccessibilityProject/AccessibilityProject/ContentView.swift
@@ -8,9 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+
+  let pictures = [
+    "ales-krivec-15949",
+    "galina-n-189483",
+    "kevin-horstmann-141705",
+    "nicolas-tissot-335096"
+  ]
+
+  @State private var selectedPicture = Int.random(in: 0...3)
+
   var body: some View {
-    Text("Hello, world!")
-      .padding()
+    Image(pictures[selectedPicture])
+      .resizable()
+      .scaledToFit()
+      .onTapGesture {
+        selectedPicture = Int.random(in: 0...3)
+      }
   }
 }
 
