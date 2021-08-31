987afef1c51f5653d74e02fd3d394bf2600eb18d
Martin Rist <mlr-github@martinrist.com>
2021-08-31 12:40:23 +0100

    Day 74 - Add empty AccessibilityProject project

---
+++ project-15-AccessibilityProject/AccessibilityProject/AccessibilityProject/AccessibilityProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilityProjectApp.swift
+//  AccessibilityProject
+//
+//  Created by Martin Rist on 31/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilityProjectApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
---
+++ project-15-AccessibilityProject/AccessibilityProject/AccessibilityProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AccessibilityProject
+//
+//  Created by Martin Rist on 31/08/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    Text("Hello, world!")
+      .padding()
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
